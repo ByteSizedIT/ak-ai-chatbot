bb1c1bb7b54e2c60360c45d9c5a96e99f4cc1da7
ByteSized <60395899+ByteSizedIT@users.noreply.github.com>
2024-03-08 12:19:14 +0000

    Add initial layout to App component

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,7 +1,32 @@
 import * as React from "react";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div className="chatbot">
+      <div className="header">
+        <div className="info-container">
+          {/* <HomePageIcon></HomePageIcon>
+            <ChatIcon></ChatIcon>
+            <LogOutIcon></LogOutIcon> */}
+        </div>
+        <div className="tick"></div>
+      </div>
+      <div className="messaging-container">
+        <div className="feed">
+          <div>
+            <div className="question bubble"></div>
+            <div className="response bubble"></div>
+          </div>
+        </div>
+        <div className="submit-container">
+          <form action="">
+            <textarea />
+            <input type="submit" value="&#x2192;" />
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
